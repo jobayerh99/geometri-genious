b69fba36f4c6f6995d2013d098cd7fe7b0b32a73
Jobayer Hosen <jobayerh99@gmail.com>
2024-04-05 02:18:56 +0600

    second commit

--- script/rectanglearea.js
+++ script/rectanglearea.js
@@ -1,3 +1,17 @@
 function calculateRectanglrArea(){
+    const weidthInput = document.getElementById('rectangle-width');
+    const widthText = weidthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
 
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    const area = width * length;
+    console.log(area);
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
